1c54106bd0e19372f504c92443d02f69f1292194
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-09-07 16:17:37 -0300

    projeto criado

---
+++ src/test/java/com/francisco/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
