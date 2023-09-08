4b0fc8844a7e6ec8dfaf28c18ef005d7a10f0d09
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-09-08 17:00:07 -0300

    interface client do feign criada

--- src/main/java/com/francisco/hexagonal/HexagonalApplication.java
+++ src/main/java/com/francisco/hexagonal/HexagonalApplication.java
@@ -2,12 +2,14 @@ package com.francisco.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HexagonalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HexagonalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalApplication.class, args);
+    }
 
 }
