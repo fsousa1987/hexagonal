af3af32c9aa30c530dbdc59127fd8574de756164
Francisco Sousa <franciscoeds1987@gmail.com>
2023-09-09 14:59:33 -0300

    repository criado

---
+++ src/main/java/com/francisco/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.francisco.hexagonal.adapters.out.repository;
+
+import com.francisco.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}
