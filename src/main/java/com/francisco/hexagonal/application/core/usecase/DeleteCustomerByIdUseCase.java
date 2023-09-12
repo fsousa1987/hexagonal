6db7e6a5fdf548d7b53772a6df4f776da8a8ecab
Francisco Sousa <franciscoeds1987@gmail.com>
2023-09-12 15:38:03 -0300

    usecase criado CRUD - DELETE

---
+++ src/main/java/com/francisco/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -0,0 +1,22 @@
+package com.francisco.hexagonal.application.core.usecase;
+
+import com.francisco.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.francisco.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+public class DeleteCustomerByIdUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
+
+    public DeleteCustomerByIdUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort,
+                                     DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
+    }
+
+    public void delete(String id) {
+        findCustomerByIdInputPort.find(id);
+        deleteCustomerByIdOutputPort.delete(id);
+    }
+
+}
---
+++ src/main/java/com/francisco/hexagonal/application/ports/out/DeleteCustomerByIdOutputPort.java
@@ -0,0 +1,7 @@
+package com.francisco.hexagonal.application.ports.out;
+
+public interface DeleteCustomerByIdOutputPort {
+
+    void delete(String id);
+
+}
